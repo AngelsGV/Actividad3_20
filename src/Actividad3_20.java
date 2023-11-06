34cb6dff0299d7e1b46f453166434fca5def93d8
AngelsGV <angels.not99@gmail.com>
2023-11-06 21:00:33 +0100

    Initial commit

--- src/Actividad3_20.java
+++ src/Actividad3_20.java
@@ -4,23 +4,17 @@ import java.util.Scanner;
 public class Actividad3_20{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double dinero; //No tiene porque tener una cantidad entera.
-        double dineroBanco, dineroBolsillos, dineroCajon, dineroCF, suma;
-        do {
-            System.out.println("¿Cuanto dinero tienes en el banco?");
-            dineroBanco = sc.nextDouble();
-            System.out.println("¿Cuanto dinero tienes en los bolsillos?");
-            dineroBolsillos = sc.nextDouble();
-            System.out.println("¿Cuanto dinero tienes en el cajón?");
-            dineroCajon = sc.nextDouble();
-            System.out.println("¿Cuanto dinero tienes en la caja fuerte?");
-            dineroCF = sc.nextDouble();
+        double dinero= 0; //No tiene porque tener una cantidad entera.
+        double suma = 0;
+        while (dinero >= 0) {
+            System.out.println("¿Cuanto dinero tienes?");
+            dinero = sc.nextDouble();
+            suma = suma + dinero;
 
-            suma = dineroBanco + dineroBolsillos + dineroCajon + dineroCF;
-
-        } while ( dineroBanco != 0 && dineroBolsillos != 0 && dineroCajon != 0 && dineroCF != 0);
-//La especificación para cerrar el bucle
-            System.out.println("El dinero total es: " + suma);
-
+            if (dinero == 0) {
+                break;  // Si el usuario ingresa 0, salimos del bucle
+            }
+        }
+        System.out.println("El dinero total es: " + suma + "€");
     }
 }
